396ff85a29140c3ec28e320b7e4fd2ede7a898be
Ivan Gavryliuk <ivan.gavryliuk@outlook.com>
2018-08-05 16:05:39 +0100

    Service Fabric package script upgrade

--- sfpackage.ps1
+++ sfpackage.ps1
@@ -1,4 +1,13 @@
-# compress existing SF package
 
+# build all
+dotnet build .\TestApp\Pluralsight.SfProd\Pluralsight.SfProd.sln --configuration release
+
+# package Service Fabric app
+msbuild .\TestApp\Pluralsight.SfProd\Pluralsight.SfProd\Pluralsight.SfProd.sfproj /p:Configuration=Release /t:Package
+
+# compress existing SF package
 Copy-ServiceFabricApplicationPackage -ApplicationPackagePath .\TestApp\Pluralsight.SfProd\Pluralsight.SfProd\pkg\Release -CompressPackage -SkipCopy
+
+# copy to deployment destination
+Copy-Item .\TestApp\Pluralsight.SfProd\Pluralsight.SfProd\pkg\ .\TestAppPkg\ -Recurse -Force
 
